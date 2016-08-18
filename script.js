7639da910058a0c232ea66650de7d5b4bc80b32b
mikimed1 <miki@tricode.co.il>
2016-08-19 01:49:52 +0300

    chapter13

---
+++ script.js
@@ -0,0 +1,41 @@
+//1 .משתנים//
+
+var x="3";
+x="miki";
+
+
+//2 .אובייקטים//
+
+var person={
+	name:"miki",
+	age: 37,
+	eyesColor: "green",
+	city: "haifa",
+	
+}
+
+person.location="israel",
+
+language:[hebrew, russian];
+
+if(person.language[0]==="hebrew"){
+	alert("true");
+}
+
+
+//3 .מערך//
+
+var numbers=[2, 4, 6, 8, 10, 12, 14, 16, 18, 20];
+
+console.log(numbers.length);
+
+console.log(numbers[0], [2], [5]);
+
+console.log(numbers.lindexOf(20));
+
+var numbers=[2, 4, 6, 8, 10, 12, 14, 16,];
+
+var numbers=["שתיים", 2, "ארבע", 4, "שש"];
+
+
+
